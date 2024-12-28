0d7cdb6a2b9ff6ec44ccca0d5d885aae3a03021b
sachinpitale22 <sachinpitale22@gmail.com>
2024-12-28 11:16:20 +0530

    Added Classic and Network LB k8s deployment

---
+++ eks.sh
@@ -0,0 +1,36 @@
+# Create Public Node Group   
+eksctl create nodegroup --cluster=eksdemo1 \
+                       --region=us-east-1 \
+                       --name=eksdemo1-ng-public1 \
+                       --node-type=t2.micro \
+                       --nodes=2 \
+                       --nodes-min=2 \
+                       --nodes-max=3 \
+                       --node-volume-size=10 \
+                       --ssh-access \
+                       --ssh-public-key=kube-demo \
+                       --managed \
+                       --asg-access \
+                       --external-dns-access \
+                       --full-ecr-access \
+                       --appmesh-access \
+                       --alb-ingress-access
+
+
+
+eksctl create nodegroup --cluster=eksdemo1 \
+                        --region=us-east-1 \
+                        --name=eksdemo1-ng-private1 \
+                        --node-type=t2.medium \
+                        --nodes-min=2 \
+                        --nodes-max=4 \
+                        --node-volume-size=12 \
+                        --ssh-access \
+                        --ssh-public-key=kube-demo \
+                        --managed \
+                        --asg-access \
+                        --external-dns-access \
+                        --full-ecr-access \
+                        --appmesh-access \
+                        --alb-ingress-access \
+                        --node-private-networking  
